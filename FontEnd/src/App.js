a9ab6e08e58bd8842d9cd47eaf7f8fa87095bf3e
BoboiboyXDoraemon <vinhnguyenquang32@gmail.com>
2022-11-25 12:02:22 +0000

    fontend

--- FontEnd/src/App.js
+++ FontEnd/src/App.js
@@ -2,16 +2,16 @@ import React, {useState} from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './view/Home'
 import Login from './view/Login'
-import { Provider } from './Context'
-
+import Users from './view/Users'
+import {Provider} from './Context'
 const App = () => {
     const [dt, setDt] = useState({})
     return <Provider value={{dt,setDt}}>
 <Routes>
     <Route path='/' element = {<Home />}/>
     <Route path='/login' element = {<Login />}/>
+    <Route path='/users' element = {<Users />}/>
 </Routes>
 </Provider>
 }
-
 export default App
