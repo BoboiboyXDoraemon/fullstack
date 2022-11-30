bbac3755c6c1be48783919c69269c89e3fea0f41
BoboiboyXDoraemon <vinhnguyenquang32@gmail.com>
2022-11-30 11:20:40 +0000

    fontend

--- FontEnd/src/App.js
+++ FontEnd/src/App.js
@@ -1,11 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './view/Home'
 import Login from './view/Login'
 import Users from './view/Users'
 import {Provider} from './Context'
+import {useBeforeUnload} from 'react-use'
+//import 
 const App = () => {
     const [dt, setDt] = useState({})
+    useEffect(()=>{
+        console.log("Start")
+    },[])
+    useBeforeUnload(()=>{
+        console.log("BeforeUnload")
+    })
     return <Provider value={{dt,setDt}}>
 <Routes>
     <Route path='/' element = {<Home />}/>
