b9a0924c475a1993885a18ad761f6eb137c4a6de
BoboiboyXDoraemon <vinhnguyenquang32@gmail.com>
2022-12-09 10:45:38 +0000

    fullstack

--- BackEnd/src/db.js
+++ BackEnd/src/db.js
@@ -8,7 +8,7 @@ const db = {
          {id:1,
         username:"long",
         password:"longpass",
-        avatar:"http://vegetable-shop.surge.sh/img/broccoli.jpg"
+        avatar:"https://vcdn1-vnexpress.vnecdn.net/2020/09/11/sh8fzyt4-159886611234720470620-5969-5840-1599809570.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=9fS0hHSZamGuZ9BYgx6Tyw"
         }
     ],
     products:[
